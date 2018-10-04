7e3b30a2fcc49b3f7472737cfdac3537527cf060
Sven Marcus <s.marcus@outlook.de>
2018-10-04 08:55:15 +0200

    Prototypes for Filter Dialog

--- QtResultVisualization/QtFilterDialog.py
+++ QtResultVisualization/QtFilterDialog.py
@@ -1,5 +1,17 @@
-from PyQt5.QtWidgets import QHeaderView, QPushButton, QSplitter, QTableWidget, \
-    QVBoxLayout, QWidget
+import PyQt5.Qt as Qt
+from PyQt5.QtWidgets import (QHeaderView, QLabel, QLineEdit, QPushButton, QSplitter,
+                             QTableWidget, QVBoxLayout, QWidget)
+
+
+class QtRowContainsFilterWidget(QWidget):
+
+    def __init__(self, parent: QWidget = None):
+        super(QtRowContainsFilterWidget, self).__init__(parent)
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        self.__textBox: QLineEdit = QLineEdit()
+        self.layout().addWidget(QLabel("Filter by content:"))
+        self.layout().addWidget(self.__textBox)
 
 
 class QtFilterWidget:
@@ -10,7 +22,7 @@ class QtFilterWidget:
         self.__currentFilters.setColumnCount(1)
         self.__currentFilters.setHorizontalHeaderLabels(["Filters"])
         self.__currentFilters.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
-        
+
         self.__addFilterButton: QPushButton = QPushButton("Add")
         self.__removeFilterButton: QPushButton = QPushButton("Remove")
 
@@ -22,3 +34,15 @@ class QtFilterWidget:
         leftWidget.layout().addWidget(self.__removeFilterButton)
 
         self.__widget.addWidget(leftWidget)
+        self.__widget.addWidget(QtRowContainsFilterWidget())
+
+    def getWidget(self) -> QWidget:
+        return self.__widget
+
+
+app: Qt.QApplication = Qt.QApplication([])
+
+widget = QtFilterWidget().getWidget()
+widget.show()
+
+app.exec_()
